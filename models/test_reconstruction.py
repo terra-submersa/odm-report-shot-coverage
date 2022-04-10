4a65a3993d31e1a150aae86b8c60ee677f64ec5d
Alexandre Masselot <alexandre.masselot@gmail.com>
2022-04-10 21:58:45 +0200

    fixed python test

--- models/test_reconstruction.py
+++ models/test_reconstruction.py
@@ -6,7 +6,7 @@ from models.reconstruction import parse_reconstruction
 class Test(TestCase):
 
     def test_json_parse_reconstruction(self):
-        got = parse_reconstruction('resources')
+        got = parse_reconstruction('example/project')
 
         self.assertEqual(60, len(got.shots), 'shots')
         self.assertEqual(48509, len(got.mesh.points), 'points')
